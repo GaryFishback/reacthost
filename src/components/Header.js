c1723f68f998f22585873d09edd33f42d569c0df
GaryFishback <gary.joel.fishback@gmail.com>
2021-08-09 01:26:41 -0500

    hello

---
+++ src/components/Header.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import App from '../'
+
+export default class Header extends React.Component{
+
+
+    render(){
+        return(
+            <div>
+                <h1>Hi there!</h1>
+            </div>
+        )
+    }
+
+}
